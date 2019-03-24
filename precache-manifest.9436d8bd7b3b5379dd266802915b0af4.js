8227e7e19db98446ed317f0a6be81c15b2271603
subinb <subin_bae@sfu.ca>
2019-03-23 22:26:44 -0700

    Updates

---
+++ precache-manifest.9436d8bd7b3b5379dd266802915b0af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PhoneBook/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eadacd478a93671e1e3d",
+    "url": "/PhoneBook/static/js/runtime~main.57ff495b.js"
+  },
+  {
+    "revision": "27ce40f4342d0f68786b",
+    "url": "/PhoneBook/static/js/main.994f9b89.chunk.js"
+  },
+  {
+    "revision": "8ddabcbd2f4e3ac7f4bb",
+    "url": "/PhoneBook/static/js/2.fab6d6e2.chunk.js"
+  },
+  {
+    "revision": "27ce40f4342d0f68786b",
+    "url": "/PhoneBook/static/css/main.26899783.chunk.css"
+  },
+  {
+    "revision": "2910646492c6d6344c3d8ddae72309de",
+    "url": "/PhoneBook/index.html"
+  }
+];
